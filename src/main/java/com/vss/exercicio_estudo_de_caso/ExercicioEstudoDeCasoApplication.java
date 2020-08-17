3bc64d5f809badc0f7b5596319c44e5624fb9952
vagnersantosdasilva <vagnersantosdasilva@gmail.com>
2020-08-17 16:27:00 -0300

    first commit

---
+++ src/main/java/com/vss/exercicio_estudo_de_caso/ExercicioEstudoDeCasoApplication.java
@@ -0,0 +1,13 @@
+package com.vss.exercicio_estudo_de_caso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioEstudoDeCasoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioEstudoDeCasoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vss/exercicio_estudo_de_caso/ExercicioEstudoDeCasoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vss.exercicio_estudo_de_caso;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExercicioEstudoDeCasoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
